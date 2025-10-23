6e286a44870c08c96c65f3dbda83a582d9273afd
Cmontano554 <colemontano554@gmail.com>
2025-10-22 21:58:42 -0700

    Delete ESP_Programmindeb_connect.php

--- ESP_Programmindeb_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "u465516230_db_sensors";
-$password = "ColeM2004!";  // your real password
-$dbname = "u465516230_Sensors";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
