37d164cb4f51b74c592a156526eeac7cce8382e3
Cmontano554 <colemontano554@gmail.com>
2025-10-22 22:00:51 -0700

    PHP Script Used to Connect to Database

---
+++ ESP_Programming/db_connect.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$username = "u465516230_db_sensors";
+$password = "";  // your real password
+$dbname = "u465516230_Sensors";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
